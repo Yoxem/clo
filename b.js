5e864363bc2d889edec9c5fe29336a8106201719
Tan Kian-ting <chenjt30@gmail.com>
2023-11-13 23:45:49 +0800

    add b.js

--- b.js
+++ b.js
@@ -9,7 +9,7 @@ let clo = new cloLib.Clo();
 
 /* CLO:  beginning of middle part*/
 clo.mainStream = /* CLO: end of middle part*/
-[`The`, `  `];
+[`In`, ` `, `the`, ` `, `beginning`, ` `, `was`, ` `, `the`, ` `, `Word,`, ` `, `and`, ` `, `the`, ` `, `Word`, ` `, `was`, ` `, `with`, ` `, `God,`, ` `, `and`, ` `, `the`, ` `, `Word`, ` `, `was`, ` `, `God.`, ` `, `The`, ` `, `same`, ` `, `was`, ` `, `in`, ` `, `the`, ` `, `beginning`, ` `, `with`, ` `, `God.`, ` `];
 /* CLO: beginning of end part*/
 clo.generatePdf();
 /*CLO : end of end part*/
